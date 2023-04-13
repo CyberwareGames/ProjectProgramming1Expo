3270e5532d326e7837dc5aee680905a9826999f4
AUSTLab1 <basselalshaar11@gmail.com>
2023-04-13 12:11:53 +0300

    half calculator

--- ProjectEXPO.cpp
+++ ProjectEXPO.cpp
@@ -1,10 +1,72 @@
 #include <iostream>
-
+#include <cmath>
 using namespace std;
 
-int main()
+double Addition(double x,double y)
 {
-    cout << "Hello, World!";
+    return x+y;
+}
 
-    return 0;
+double Subtraction(double x, double y)
+{
+    return x-y;
+}
+
+double Multiplication(double x, double y)
+{
+    return x*y;
+}
+
+double Division(double x, double y)
+{
+    if (y==0)
+    {
+        cout << "Syntax Error"<<endl;
+        return -1;
+    }
+    else
+        return x/y;
+}
+
+double Percentage(double x, double y)
+{
+    return (x*y)/100;
+}
+
+double Square_root(double x)
+{
+    if(x<0)
+    {
+        cout<<"Math Error";
+        return -1;
+    }
+}
+
+double Exponent()
+{
+
+}
+
+void Calculator()
+{
+
+}
+
+void Hangman()
+{
+
+}
+
+void TicTacToe()
+{
+
+}
+
+void Stop_Watch()
+{
+
+}
+
+int main(){
+
 }
